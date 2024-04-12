e70c8a25c0f67eba81ea46856c8f7c3084a814a6
Felpa14 <felipegabriel63@gmail.com>
2024-04-12 13:56:49 -0300

    Back-End pagina cavalos e alguns ajustes.

--- Models/Cavalo.cs
+++ Models/Cavalo.cs
@@ -1,13 +1,16 @@
-﻿namespace HipicaFacilSQL.Models
+﻿using System.Drawing;
+
+namespace HipicaFacilSQL.Models
 {
     public class Cavalo
     {
         public int ID { get; set; }
-        public string ?Nome { get; set; }
-        public string ?Raca { get; set; }
-        public DateTime ?Idade { get; set; }
-        public double ?Peso { get; set; }
+        public string Nome { get; set; }
+        public string Raca { get; set; }
+        public string Idade { get; set; }
+        public string Peso { get; set; }
+        public string ?Imagem { get; set; }
 
-        public ICollection<Navigation> Navigations { get; set; }
+        public ICollection<Navigation> ?Navigations { get; set; }
     }
 }
